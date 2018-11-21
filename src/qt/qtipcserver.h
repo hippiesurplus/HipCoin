a963191568693add71364d73f7be1b8e2c5c8e90
jacob <jacob@hazedhosting.com>
2018-11-21 11:55:33 -0800

    First commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define HipCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "HipCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
